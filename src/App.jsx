097a339e703e325c19696484182cc6b4905b5256
Tolexy95 <toyin95.olubayo@gmail.com>
2024-02-16 11:40:55 +0100

    work still ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { Routes, Route } from "react-router-dom";
 
 //pages
 import Home from "./pages/Home";
+import AboutPage from "./pages/About";
+import CoursesPage from "./pages/Courses";
+import PartnerPage from "./pages/Partners";
+import ServicesPage from "./pages/Services";
+import ContactPage from "./pages/Contact";
+
 
 
 function App() {
@@ -10,6 +16,12 @@ function App() {
     <> 
       <Routes>
         <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/signUp" element={<AboutPage/>} />
+        <Route exact path="/signUp" element={<CoursesPage/>} />
+        <Route exact path="/signUp" element={<ContactPage/>} />
+        <Route exact path="/signUp" element={<ServicesPage/>} />
+        <Route exact path="/signUp" element={<PartnerPage/>} />
+
         {/* <Route  path= "*" element={<NotFoundPage/>}/> */}
       </Routes>
       
---
+++ src/pages/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServicesPage = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-4">Our Services</h1>
+      {/* Add additional content here */}
+    </div>
+  );
+};
+
+export default ServicesPage;
