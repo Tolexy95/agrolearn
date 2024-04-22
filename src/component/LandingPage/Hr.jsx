f7d707afa0ac63eb9cf13a92000992cd8238eeb7
Tolexy95 <toyin95.olubayo@gmail.com>
2024-04-22 16:27:33 +0100

    work in progress

--- src/component/LandingPage/Hr.jsx
+++ src/component/LandingPage/Hr.jsx
@@ -1,85 +1,18 @@
 import React from 'react'
-import picture from '../../assets/two girls.jpg'
-import picture2 from '../../assets/paw paw man.jpg'
-import { GiPitchfork } from "react-icons/gi";
-import Button from '../Button/CustomButton';
-import { ButtonSize, ButtonState } from '../Button/ButtonStyles';
+import AboutComponent from './AboutComponent';
 
 
 const Hr = () => {
     return (
         <>
-            <div className={`font-inter px-5 md:px-12 mt-12 flex  flex-col lg:flex-row gap-5 mb-36 lg:gap-11 lg:p-10 relative`}>
+            <AboutComponent
 
-                <img src="https://www.agrohive.co/assets/images/icons/about-bg-icon-1-1.png " className="absolute top-[95%] md:top-[90%] lg:top-[70%]  left-0 -z-10" alt="" />
-
-
-                <div className="md:flex md:relative lg:items-start w-full">
-                    <div>
-                        <img src={picture} className="rounded-md lg:ml-12" alt="" />
-                    </div>
-
-                    <div className='md:w-[520px] mt-5 md:mt-0 md:absolute md:top-[75%] md:left-[30%] lg:top-[52%] lg:left-[-38px]'>
-                        <img
-                            src={picture2}
-                            className="rounded-md"
-                            alt=""
-                        />
-                    </div>
-                </div>
-
-                <div className='md:px-5  lg:p-24 lg:w-full'>
-                    <h1 className='mt-9 font-semibold text-[#9ea5a2] md:mt-52 lg:mt-0 text-sm mb-3 tracking-[0.2em]' style={{ lineHeight: '1.3' }}>WELCOME TO AGROHIVE</h1>
-
-                    <h2 className="text-2xl md:text-[35px] lg:text-[2.8rem] text-[#255946] font-semibold leading-loose" style={{ lineHeight: '1.3' }}>
-                        Easy HR staffing for AgriBusinesses
-                    </h2>
-
-                    <p className='mt-6 text-[18px]  text-[#00cc3c] font-sora italic'>Africa's first AgriTech human resources and work force management company</p>
-                    <div>
-                        <p className='mt-6 text-base text-[#6e7673] tracking-widest' style={{ lineHeight: '1.9' }}>We assist thousands of agri-food businesses across Africa to upskill, hire & manage skilled and trusted agricultural workforces using technology. We know that increasing agricultural productivity and profitability starts with easy recruiting and managing a team of skilled and trusted agricultural experts.</p>
-
-
-                        <p className='mt-5 text-base font-bold text-[#6c757d] mb-10'>Our goal is to simplify this process, while you focus on increasing your yields.</p>
-                    </div>
-
-
-                    <hr className='text-green-500 text-2xl' />
-
-                    <div className='lg:flex lg:flex-row items-center lg:mt-8 mt-4'>
-
-                        <div className='mt-10 lg:mt-0 flex items-center px-4 gap-4   '>
-                            <GiPitchfork className='' />
-                            <p className='text-[#176300] font-semibold text-2xl'>Agro Experts</p>
-                        </div>
-
-                        <div className='mt-6 lg:mt-0 lg:mb-0 mb-10 flex items-center px-4  gap-4'>
-                            <GiPitchfork className='' />
-                            <p className='text-[#176300] text-2xl font-semibold'>Professional farmers</p>
-                        </div>
-
-                    </div>
-
-                    <hr className='mt-3 text-green-500 text-2xl  lg:mt-8 ' />
-
-                    <div className='
-                    flex flex-col gap-1 md:flex-row items-center md:gap-5 mt-4 md:mt-7'>
-
-                        <Button
-                            value='Hire Experts'
-                            size={ButtonSize.md}
-                            variant={ButtonState.PRIMARY}
-                        />
-
-                        <Button
-                            className={`${ButtonSize ? 'w-48 md:w-1/2' : 'w-1/2'}`}
-                            value='Apply as an Expert'
-                            size={ButtonSize.md}
-                            variant={ButtonState.OUTLINE}
-                        />
-                    </div>
-                </div>
-            </div>
+                title="WELCOME TO AGROHIVE"
+                subtitle=" Easy HR staffing for AgriBusinesses"
+                description1="Africa's first AgriTech human resources and work force management company"
+                description2="We assist thousands of agri-food businesses across Africa to upskill, hire & manage skilled and trusted agricultural workforces using technology. We know that increasing agricultural productivity and profitability starts with easy recruiting and managing a team of skilled and trusted agricultural experts."
+                description3="Our goal is to simplify this process, while you focus on increasing your yields."
+            />               
         </>
 
     )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import AboutComponent from '../component/LandingPage/AboutComponent';
 
 
 const AboutPage = () => {
   return (
-    <div className="container mx-auto py-8">
+    <div className="mt-28">
       <h1 className="text-3xl font-bold mb-4 text-center justify-center ">About Us</h1>
 
-
+      <AboutComponent
+        subtitle="Our Solution"
+        description2="AgroHive, Africa’s fast growing human resource and workforce management agritech company, is boldly tackling one of the major challenges encountered by agri-food businesses across Africa, which is to have easy access to blended agricultural training, a consistent supply of appropriately trained agricultural workforce that meets their skills needs and a digital workforce management platform to manage and track the performance of their staff."
+        description4="We collaborate with key stakeholders in the agricultural industry to train, upskill and certify agricultural talents. Once certified, agricultural talents are outsourced to agribusinesses through AgroHive’s digital hiring platform."
+      />
     </div>
   );
 };
